af867d45491a1de287f8f32238b8eb892f4133b0
GuilhermeViana14 <guilhermevianafilgueiras@gmail.com>
2024-09-23 14:17:53 -0300

    feito o inicio do site a navbar o titulo de indrotução e a primeira parte do sobre mim

--- meu-site/src/App.js
+++ meu-site/src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
+import Navbar from './Navbar';
 import './App.css';
-
+import Sobremim from './sobre-mim';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <div >
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css"></link>
+    <Navbar /> {/* Incluindo a Navbar */}
+
+    <header className='titulo'>
+      <h1>Guilherme<br></br>  viana</h1>
+    </header>
+    
+    <div className="sub-titulo">
+      <p>Redes Sociais:</p>
+      <div className="social-icons">
+        <a href="https://www.instagram.com/gui.viana14/?next=%2F" className="icon-link" aria-label="Instagram"><i className="fab fa-instagram"></i></a>
+        <a href="https://www.linkedin.com/in/guilherme-viana-filgueiras-4b5211234/" className="icon-link" aria-label="LinkedIn"><i className="fab fa-linkedin-in"></i></a>
+        <a href="https://github.com/GuilhermeViana14" className="icon-link" aria-label="GitHub"><i className="fab fa-github"></i></a>
+      </div>
     </div>
+
+    <Sobremim /> {/* Incluindo sobremim */}
+
+
+
+  </div>
   );
 }
 
---
+++ meu-site/src/sobre-mim.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './sobre-mim.css';
+
+const Sobremim = () => {
+    return (
+        <div>
+            <h1 className="titulo_sobremim">Sobre Mim -</h1>
+            <div className="container-sobremim">
+                <p className="texto-sobremim">
+                    Meu nome é Guilherme Viana e minha paixão por tecnologia começou cedo, especialmente com jogos, que despertaram meu interesse em seguir uma carreira na área de TI. 
+                    Foi esse fascínio que me levou a escolher o curso de Ciência da Computação, onde posso aprofundar meus conhecimentos e trabalhar com aquilo que mais me motiva.
+                </p>
+
+                <p className="texto-sobremim">
+                    Este é o segundo bloco de texto. Aqui você pode incluir mais informações, projetos, experiências ou qualquer outro conteúdo que deseje destacar dentro de uma moldura com borda.
+                </p>
+            </div>
+        </div>
+    );
+};
+
+export default Sobremim;
